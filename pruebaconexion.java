e90335ac189a6ec036ad7f4682300e4de3e99507
Marco <marcom4drigal@gmail.com>
2021-05-23 21:48:57 -0600

    Se agrega método para obtener los personajes existentes, con sus accesorios

--- pruebaconexion.java
+++ pruebaconexion.java
@@ -1,35 +1,64 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package testjavaprolog;
 import java.util.Map;
 import org.jpl7.*;
 
-/**
- *
- * @author marco
- */
 public class Testjavaprolog {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
         
-        Query q = new Query("consult('outfoxed.pl')");
+        // Al parecer hay que ejecutar siempre estas 2 líneas antes de realizar consultas
+        // Si no, da un error
+        Query q = new Query("consult('outfoxed.pl')"); 
         q.hasSolution();
         
+        obtenerPersonajes();
+        
+        // Esto permite hacer una consulta a cierta relación, retorna si tiene solución
         q = new Query("tiene_accesorio(emily, reloj)");
         System.out.println(q.hasSolution());
+
         
+        /*
+        // Esta part itera sobre un conjunto de datos
         Map<String, Term>[] res = q.allSolutions();
         
         for(int i = 0; i<res.length; i++){
             System.out.println(res[i]);
+        }*/
+    }
+    
+    // Este método retorna los personajes como un HashMap
+    public static int obtenerPersonajes(){
+        
+        Query personajes = new Query("personaje(Persona, X, Y, Z)");
+        
+        Map<String, Term>[] listaPersonajes = personajes.allSolutions();
+        
+        for(int i = 0; i<listaPersonajes.length; i++){
+            System.out.println(listaPersonajes[i]);
+            
         }
+        /*
+        Variable X = new Variable("X");
+        Variable Y = new Variable("Y");
+        Variable Z = new Variable("Z");
+        Variable nombre = new Variable("Persona");
+        Query personajes = new Query("personaje", new Term[] {nombre, X, Y, Z});
+        
+        Map<String, Term>[] solutions;
+        solutions = personajes.allSolutions();
+        
+        
+        
+        for(int i = 0; i< solutions.length; i++){
+            System.out.println(solutions[i].get("X"));
+        }
+     */
+        return 0;
     }
     
 }
